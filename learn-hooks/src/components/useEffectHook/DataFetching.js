4d20ae8ff0958b4cdf06cd5fb3d2ebf4113c89b7
rohith goli <rohith.goli@anywhere.co>
2023-01-29 22:08:48 +0530

    useEffect - fetching data based on input

--- learn-hooks/src/components/useEffectHook/DataFetching.js
+++ learn-hooks/src/components/useEffectHook/DataFetching.js
@@ -3,26 +3,24 @@ import axios from 'axios'
 
 function DataFetching() {
 
-    const [posts, setPosts] = useState([])
+    const [post, setPost] = useState({})
+    const [id, setId] = useState(1)
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then(res => {
             console.log(res)
-            setPosts(res.data)
+            setPost(res.data)
         })
         .catch(err => {
             console.log(err)
         })
-    }, [])
+    }, [id])
 
     return(
         <div>
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
-                ))}
-            </ul>
+            <input type="text" value={id} onChange={e => setId(e.target.value)}/>
+            <div>{post.title}</div>
         </div>
     )
 }
