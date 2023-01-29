095b992a39bef15b720bfdc3394ab8afaf7170e4
rohith goli <rohith.goli@anywhere.co>
2023-01-29 17:27:51 +0530

    conditional run effects

--- learn-hooks/src/components/useEffectHook/ClassCounterOne.js
+++ learn-hooks/src/components/useEffectHook/ClassCounterOne.js
@@ -4,7 +4,8 @@ class ClassCounterOne extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            count: 0
+            count: 0,
+            name: ''
         }
     }
 
@@ -13,12 +14,20 @@ class ClassCounterOne extends Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        document.title = `Clicked ${this.state.count} times`
+        if (prevState.count !== this.state.count) {
+            console.log('Updating document title')
+            document.title = `Clicked ${this.state.count} times`
+        }
     }
 
     render() {
         return (
             <div>
+                <input 
+                    type='text'
+                    value={this.state.name}
+                    onChange={e => this.setState({...this.state, name: e.target.value})}
+                />
                 <button onClick={() => this.setState({count: this.state.count + 1}) }>Count: {this.state.count}</button>
             </div>
         )
--- learn-hooks/src/components/useEffectHook/HookCounterOne.js
+++ learn-hooks/src/components/useEffectHook/HookCounterOne.js
@@ -3,13 +3,19 @@ import React, {useState, useEffect} from 'react';
 function HookCounterOne() {
 
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
 
     useEffect(() => {
+        console.log('Updating document title - useEffect')
         document.title = `You Clicked ${count} times`
-    })
+    }, [count])
 
     return(
         <div>
+            <input type='text'
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
             <button onClick={() => setCount(count + 1)}>Click Count: {count}</button>
         </div>
     )
