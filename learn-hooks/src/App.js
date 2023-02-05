5fcfa52d68981f10c1e36bb74de7603872bbad25
rohith goli <rohith.goli@anywhere.co>
2023-02-05 23:22:04 +0530

    useReducer with useContext for Global State Mgmt

--- learn-hooks/src/App.js
+++ learn-hooks/src/App.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, {useReducer} from 'react'
 import './App.css';
-import CounterOne from './components/CounterOne';
+import ComponentA from './components/ComponentA';
+import ComponentB from './components/ComponentB';
+import ComponentC from './components/ComponentC';
+
 import CounterTwo from './components/CounterTwo';
 import CounterThree from './components/CounterThree';
 
+export const CountContext = React.createContext()
+
+const initialState = 0
+const reducer = (state, action) => {
+    switch(action) {
+        case 'increment':
+            return state + 1
+        case 'decrement':
+            return state - 1
+        case 'reset':
+            return initialState
+        default:
+            return state
+    }
+}
+
 function App() {
+
+  const [count, dispatch] = useReducer(reducer, initialState)
+
   return (
-    <div className="App">
-      {/* <CounterOne /> */}
-      {/* <CounterTwo /> */}
-      <CounterThree />
-    </div>
+    <CountContext.Provider 
+      value={{countState: count, countDispatch: dispatch}}
+    >
+      <div className="App">
+        {/* <CounterOne /> */}
+        {/* <CounterTwo /> */}
+        {/* <CounterThree /> */}
+        <div>Count - {count}</div>
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </div>
+    </CountContext.Provider>
   );
 }
 
