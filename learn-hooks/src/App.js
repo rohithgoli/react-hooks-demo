c8ad53f764f22bd332b6ed52b35d5e1c7fbcc159
rohith goli <rohith.goli@anywhere.co>
2023-02-12 23:54:58 +0530

    useInput custom hook

--- learn-hooks/src/App.js
+++ learn-hooks/src/App.js
@@ -4,14 +4,16 @@ import CounterOne from './components/CounterOne'
 import CounterTwo from './components/CounterTwo'
 import DocTitleOne from './components/DocTitleOne'
 import DocTitleTwo from './components/DocTitleTwo'
+import UserForm from './components/UserForm'
 
 function App() {
   return (
     <div>
-      <CounterOne />
-      <CounterTwo />
+      {/* <CounterOne />
+      <CounterTwo /> */}
       {/* <DocTitleOne />
       <DocTitleTwo /> */}
+      <UserForm />
     </div>
   )
 }
