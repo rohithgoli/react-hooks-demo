f22a65307dfc740063ca9f890e7c19f3f521e2d9
rohith goli <rohith.goli@anywhere.co>
2023-02-12 20:09:12 +0530

    useRef intro & access DOM nodes

--- learn-hooks/src/App.js
+++ learn-hooks/src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.css'
-import Counter from './components/Counter'
+import FocusInput from './components/FocusInput'
 
 
 function App() {
   return (
     <div>
-      <Counter />
+      <FocusInput />
     </div>
   )
 }
