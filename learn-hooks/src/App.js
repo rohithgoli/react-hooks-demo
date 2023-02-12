cff70e7e18c373a03972c20e0fa9068c4147ded8
rohith goli <rohith.goli@anywhere.co>
2023-02-12 19:22:21 +0530

    useMemo hook

--- learn-hooks/src/App.js
+++ learn-hooks/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
-import ParentComponent from './components/ParentComponent'
+import Counter from './components/Counter'
+
 
 function App() {
   return (
-    <div className="App">
-      <ParentComponent />
+    <div>
+      <Counter />
     </div>
   )
 }
