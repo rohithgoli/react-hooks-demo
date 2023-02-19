0fe58933bf03a846ffb5294083f9935be04d77c3
rohith goli <rohith.goli@anywhere.co>
2023-02-19 17:40:23 +0530

    render-props pattern intro

--- learn-hooks/src/App.js
+++ learn-hooks/src/App.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react'
 import './App.css'
 import ClickCounter from './components/ClickCounter'
 import HoverCounter from './components/HoverCounter'
+import ClickCounterTwo from './components/ClickCounterTwo'
+import HoverCounterTwo from './components/HoverCounterTwo'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ClickCounter name="EverEvolving Dev"/>
-        <HoverCounter />
+        <ClickCounterTwo />
+        <HoverCounterTwo />
       </div>
     )
   }
