e0eb5483ab4fd24d3a92decf591d1f9a7eff1574
rohith goli <rohith.goli@anywhere.co>
2023-02-19 22:45:23 +0530

    react context api

--- learn-hooks/src/App.js
+++ learn-hooks/src/App.js
@@ -1,29 +1,14 @@
 import React, {Component} from 'react'
 import './App.css'
-import ClickCounter from './components/ClickCounter'
-import HoverCounter from './components/HoverCounter'
-import ClickCounterTwo from './components/ClickCounterTwo'
-import HoverCounterTwo from './components/HoverCounterTwo'
-import User from './components/User'
-import Counter from './components/Counter'
-
+import ComponentC from './components/ComponentC'
+import { UserProvider } from './components/userContext'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter 
-          render={(count, incrementCount) => (
-            <ClickCounterTwo count={count} incrementCount={incrementCount}/>
-          )}
-        />
-        <Counter 
-          render={(count, incrementCount) => (
-            <HoverCounterTwo  count={count} incrementCount={incrementCount}/>
-          )}
-        />
-        {/* <ClickCounterTwo />
-        <HoverCounterTwo />
-        <User render={(isLoggedIn) => isLoggedIn ? "EverEvolving Dev" : "Guest"}/> */}
+        <UserProvider value="EverEvolving Dev">
+          <ComponentC />
+        </UserProvider>
       </div>
     )
   }
