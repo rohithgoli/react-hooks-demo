c72108758ba0e552e5fa8e35878dcfadeb4e3161
rohith goli <rohith.goli@anywhere.co>
2023-02-19 18:31:02 +0530

    render prop pattern example

--- learn-hooks/src/App.js
+++ learn-hooks/src/App.js
@@ -5,14 +5,25 @@ import HoverCounter from './components/HoverCounter'
 import ClickCounterTwo from './components/ClickCounterTwo'
 import HoverCounterTwo from './components/HoverCounterTwo'
 import User from './components/User'
+import Counter from './components/Counter'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ClickCounterTwo />
+        <Counter 
+          render={(count, incrementCount) => (
+            <ClickCounterTwo count={count} incrementCount={incrementCount}/>
+          )}
+        />
+        <Counter 
+          render={(count, incrementCount) => (
+            <HoverCounterTwo  count={count} incrementCount={incrementCount}/>
+          )}
+        />
+        {/* <ClickCounterTwo />
         <HoverCounterTwo />
-        <User render={(isLoggedIn) => isLoggedIn ? "EverEvolving Dev" : "Guest"}/>
+        <User render={(isLoggedIn) => isLoggedIn ? "EverEvolving Dev" : "Guest"}/> */}
       </div>
     )
   }
